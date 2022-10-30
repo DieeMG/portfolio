7902a6cb26a901881e7b865c9e6f7036b268fb2d
Diego <dmgelvez@gmail.com>
2022-10-30 00:52:07 -0300

    final final fix

--- actionForm2.php
+++ actionForm2.php
@@ -16,41 +16,39 @@ $json = json_decode($response, true);
 $ok = $json['success'];
 $score = $json['score'];
 
-if ( $ok === false ) {
+if ( $ok === false && $score < 0.7 ) {
 
     echo "El mensaje no pudo ser enviado";
     die();
-
-} 
-
-if (  $score < 0.7 ) {
-    
-    echo "El mensaje no pudo ser enviado";
-    die();
-}
-        
-if ( ! $email == '' && ! $name == '' && ! $project == '' ) {
-
-    if ( filter_var($email, FILTER_VALIDATE_EMAIL) ) {
-
-        $receiver = 'hola@diegogelvez.ar';
-        $subject = "Contacto Web | $name < $email >";
-        $sender = 'From: ' . $email;
-        
-        $body = "Contacto desde Home diegogelvez.ar \n\nNombre: $name\nE-Mail: $email\n\nProyecto: $project\n";
-        
-        if ( mail($receiver, $subject, $body, $sender) ) {
-            echo "Mensaje enviado correctamente";
-        } else {
-            echo "Hubo un problema al enviar el mensaje";
-        }
-        
-    } else {
-        echo "Por favor ingresá un email valido";
-    }
 
 } else {
-    echo "Por favor completá todos los campos";
+    
+    if ( ! $email == '' && ! $name == '' && ! $project == '' ) {
+
+        if ( filter_var($email, FILTER_VALIDATE_EMAIL) ) {
+    
+            $receiver = 'hola@diegogelvez.ar';
+            $subject = "Contacto Web | $name < $email >";
+            $sender = 'From: ' . $email;
+            
+            $body = "Contacto desde Home diegogelvez.ar \n\nNombre: $name\nE-Mail: $email\n\nProyecto: $project\n";
+            
+            if ( mail($receiver, $subject, $body, $sender) ) {
+                echo "Mensaje enviado correctamente";
+            } else {
+                echo "Hubo un problema al enviar el mensaje";
+            }
+            
+        } else {
+            echo "Por favor ingresá un email valido";
+        }
+    
+    } else {
+        echo "Por favor completá todos los campos";
+    }
+
 }
+       
+
 
 ?>
