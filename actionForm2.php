536cf3d85246542815f4a8c2eb4323f0746c9177
Diego <dmgelvez@gmail.com>
2022-10-29 23:17:01 -0300

    test

--- actionForm2.php
+++ actionForm2.php
@@ -14,34 +14,6 @@ $json = json_decode($response, true);
 $ok = $json['success'];
 $score = $json['score'];
 
-if ( $ok && $score < 0.7 ) {
 
-    if ( ! $email == '' && ! $name == '' && ! $project == '' ) {
-
-        if ( filter_var($email, FILTER_VALIDATE_EMAIL) ) {
-    
-            $receiver = 'hola@diegogelvez.ar';
-            $subject = "Contacto Web | $name < $email >";
-            $sender = 'From: ' . $email;
-            
-            $body = "Contacto desde Home diegogelvez.ar \n\nNombre: $name\nE-Mail: $email\n\nProyecto: $project\n";
-            
-            if ( mail($receiver, $subject, $body, $sender) ) {
-                echo "Mensaje enviado correctamente";
-            } else {
-                echo "Hubo un problema al enviar el mensaje";
-            }
-            
-        } else {
-            echo "Por favor ingresá un email valido";
-        }
-    
-    } else {
-        echo "Por favor completá todos los campos";
-    }
-
-} else {
-    echo "El mensaje no pudo ser enviado";
-}
 
 ?>
